4b7f0c77e26e9d029910b8ab8afc37e7d2b3da4c
alejandro <alejandro97.acm@gmail.com>
2022-10-17 14:52:51 -0400

    getting orcid list from assets and ldap names

--- spi/tasks/orcid.py
+++ spi/tasks/orcid.py
@@ -1,14 +1,18 @@
-import requests
-import json
-
 from spi.controllers import PersonsController
 from spi.database import connect
 from spi.tasks.ad import get_ldap_list_persons
+from spi.controllers import OrcidController
+
+import requests
+import json
+import time
+from random import randint
 
 ORCID_API = 'https://pub.orcid.org/v3.0/expanded-search/'
+ASSETS_JSON_TMP = "../../data/apiassets.jsonld"
 
 
-def get_orcid_list_by_assets_name(org: str = '') -> str:
+def get_orcid_list_by_name(given_names: str = '', family_name: str = '') -> str:
     """
         Return a list of persons according to the organization
         @params:
@@ -26,29 +30,79 @@ def get_orcid_list_by_assets_name(org: str = '') -> str:
     #          'Río" OR "Hermanos Saiz Montes de Oca") OR email:*@upr.edu.cu'
     #     }
     params = {
-        'q': '(given-names:Rafael) AND (family-name:Martinez Estevez)'
+        'q': f'(given-names:{given_names.lower()}) AND (family-name:{family_name.lower()})'
     }
     response = requests.get(ORCID_API, headers=headers, params=params)
     # return response.text
-    return json.loads(response.text)['expanded-result']
+    if response.text and 'expanded-result' in json.loads(response.text).keys():
+        print('success')
+        return json.loads(response.text)['expanded-result']
+    return None
 
 
-def get_orcid_list_by_ad_name(given_names: str = '', family_name: str = '') -> str:
-    headers = {
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/102.0.0.0 Safari/537.36',
-        'Accept': 'application/json'
-    }
-    params = {
-        'q': '(given-names:Rafael) AND (family-name:Martinez Estevez)'
-    }
-    response = requests.get(ORCID_API, headers=headers, params=params)
-    return json.loads(response.text)['expanded-result']
+async def get_orcid_list():
+    orcid_list_cleaned = []
 
+    file = open(ASSETS_JSON_TMP, "r")
+    persons_assets = json.loads(file.read())
 
-def get_orcid_list() -> str:
-    orcid_list_by_assets_name = get_orcid_list_by_assets_name()
-    orcid_list_by_ad_name = get_orcid_list_by_ad_name()
+    # ldap_list = get_ldap_list_persons()[0:5]
+    # assets_list = persons_assets["hydra:member"][0:5]
+
+    ldap_list = get_ldap_list_persons()
+    assets_list = persons_assets["hydra:member"]
+
+    for item in assets_list:
+        name = item['nombre']
+        lastName = item['apellido1'] + ' ' + item['apellido2']
+        assets_full_name = name + ' ' + lastName
+
+        # wait a time before execute query for assets orcid
+        sleep_time = randint(3, 9)
+        print('sleep {0} seconds'.format(sleep_time))
+        time.sleep(sleep_time)
+        orcid_assests_list = get_orcid_list_by_name(name, lastName)
+
+        if orcid_assests_list:
+            for orcid_item in orcid_assests_list:
+                # if orcid_item and 'given-names' in orcid_item.keys() and 'family-names' in orcid_item.keys():
+                #     given_names = orcid_item['given-names'] if orcid_item['given-names'] else ''
+                #     family_names = orcid_item['family-names'] if orcid_item['family-names'] else ''
+                #     orcid_full_name = given_names + ' ' + family_names
+
+                    # if orcid_item['orcid-id'] not in orcid_list_cleaned and orcid_full_name.lower() == assets_full_name.lower():
+                    if orcid_item['orcid-id'] not in orcid_list_cleaned:
+                        orcid_list_cleaned.append(orcid_item['orcid-id'])
+                        await OrcidController.insert(orcid_item)
+
+    for dn, entry in ldap_list:
+        if entry and 'sn' in entry.keys() and 'givenName' in entry.keys() and 'displayName' in entry.keys():
+            given_names = entry['givenName'][0].decode("utf-8")
+            family_name = entry['sn'][0].decode("utf-8")
+
+            # wait a time before execute query for ldap orcid
+            sleep_time = randint(3, 9)
+            print('sleep {0} seconds'.format(sleep_time))
+            time.sleep(sleep_time)
+            orcid_ldap_list = get_orcid_list_by_name(given_names, family_name)
+
+            if orcid_ldap_list:
+                for orcid_item in orcid_ldap_list:
+                    # if orcid_item and 'given-names' in orcid_item.keys() and 'family-names' in orcid_item.keys():
+                        # given_names = orcid_item['given-names'] if orcid_item['given-names'] else ''
+                        # family_names = orcid_item['family-names'] if orcid_item['family-names'] else ''
+
+                        # orcid_full_name = given_names + ' ' + family_names
+                        # ldap_displayName = entry['displayName'][0].decode("utf-8")
+
+                        # if orcid_item['orcid-id'] not in orcid_list_cleaned and orcid_full_name == ldap_displayName:
+                        if orcid_item['orcid-id'] not in orcid_list_cleaned:
+                            orcid_list_cleaned.append(orcid_item['orcid-id'])
+                            await OrcidController.insert(orcid_item)
+
+    f_ad = open("demofile4.json", "a")
+    f_ad.write(str(orcid_list_cleaned))
+    f_ad.close()
 
 
 async def get_orcid():
@@ -59,7 +113,7 @@ async def get_orcid():
         orcid_list = get_orcid_list()
         print(fullNameSPI)
         for item in orcid_list:
-            fullNameORCID = item['given-names'] + ' ' + item['family-names']
+            fullNameORCID = item['given-names'] + ' ' + item['family-name']
             print(fullNameORCID)
             if fullNameSPI.lower() == fullNameORCID.lower():
                 print(item)
@@ -73,4 +127,5 @@ if __name__ == '__main__':
     import asyncio
 
     asyncio.run(connect())
-    asyncio.run(get_orcid())
+    # asyncio.run(get_orcid())
+    asyncio.run(get_orcid_list())
