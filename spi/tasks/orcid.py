a16f8700d2721ca92b654715b403e2901a5ebc8a
alejandro <alejandro97.acm@gmail.com>
2022-10-25 13:38:50 -0400

    reformat code and add match case for email

--- spi/tasks/orcid.py
+++ spi/tasks/orcid.py
@@ -4,22 +4,20 @@ import json
 import time
 from random import randint
 
-from spi.controllers import PersonsController
+from spi.controllers import PersonsController, OrcidController
 from spi.database import connect
-from spi.tasks.ad import get_ldap_list_persons
 from spi.controllers import OrcidController
 
 ORCID_API = str(os.getenv("ORCID_API"))
 ASSETS_JSON_TMP = str(os.getenv("ASSETS_JSON_TMP"))
 
 
-def get_orcid_list_by_name(given_names: str = '', family_name: str = '') -> str:
+def get_orcid_list_by_name_and_last_name(given_names: str = '', family_name: str = '') -> str:
     """
         Return a list of persons according to the organization
         @params:
-            `org`: is a `str` with the alias of the organization with the format:
-            `alias+operator(AND,OR)+alias`
-                   example:  "Universidad de Pinar del Rio"+OR+UPR
+            `given-names`: is a `str` with the first name of the person
+            `family-names`: is a `str` with the lastt name of the person
     """
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -33,82 +31,133 @@ def get_orcid_list_by_name(given_names: str = '', family_name: str = '') -> str:
     params = {
         'q': f'(given-names:{given_names.lower()}) AND (family-name:{family_name.lower()})'
     }
-    response = requests.get(ORCID_API, headers=headers, params=params)
+    response = requests.get(f'{ORCID_API}/expanded-search/', headers=headers, params=params)
     # return response.text
     if response.text and 'expanded-result' in json.loads(response.text).keys():
-        print('success')
+        print('GET ORCID LIST BY NAME AND LAST NAME')
+        print("=========================")
         return json.loads(response.text)['expanded-result']
     return None
 
+def get_orcid_list_by_full_name(full_name: str = '') -> str:
+    """
+        Return a list of persons according to the organization
+        @params:
+            `given-and-family-names`: is a `str` with the first name of the person
+    """
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/102.0.0.0 Safari/537.36',
+        'Accept': 'application/json'
+    }
+    params = {
+        'q': f'(given-and-family-names:{full_name.lower()})'
+    }
+    response = requests.get(f'{ORCID_API}/expanded-search/', headers=headers, params=params)
+    # return response.text
+    if response.text and 'expanded-result' in json.loads(response.text).keys():
+        print('GET ORCID LIST BY FULL NAME')
+        print("=========================")
+        return json.loads(response.text)['expanded-result']
+    return None
+
+def get_email_by_orcid(orcid: str = '') -> str:
+    """
+        Return info of email according to an orcid
+    """
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/102.0.0.0 Safari/537.36',
+        'Accept': 'application/json'
+    }
+    response = requests.get(f'{ORCID_API}/{orcid}/email/', headers=headers)
+    # return response.text
+    if response.text and 'email' in json.loads(response.text).keys():
+        print('GET ORCID PERSON BY EMAIL')
+        print("=========================")
+        emails = []
+        for email in json.loads(response.text)['email']:
+            emails.append(email['email'])
+        return emails
+    return []
+
+
+# if exist update a person with the orcid_id from orcid api
+async def update_orcid_by_person(person_id, orcid_list):
+    if orcid_list:
+        for orcid_item in orcid_list:
+            if orcid_item['given-names']:
+                given_names = orcid_item['given-names']
+            else:
+                given_names = ''
+
+            if orcid_item['family-names']:
+                family_names = orcid_item['family-names']
+            else:
+                family_names = ''
+
+            full_name = given_names + ' ' + family_names
+
+            # if orcid_item and 'given-names' in orcid_item.keys() and 'family-names' in orcid_item.keys():
+            # given_names = orcid_item['given-names'] if orcid_item['given-names'] else ''
+            # family_names = orcid_item['family-names'] if orcid_item['family-names'] else ''
+
+            # orcid_full_name = given_names + ' ' + family_names
+            # ldap_displayName = entry['displayName'][0].decode("utf-8")
+
+            # if orcid_item['orcid-id'] not in orcid_list_cleaned and orcid_full_name == ldap_displayName:
+
+            existent_orcid_item = await OrcidController.retrieve_one({'orcid_id': orcid_item['orcid-id']})
+            if not existent_orcid_item:
+                print('INSERT ORCID PERSON')
+                print("=========================")
+                orcid_item.update({
+                    "full_name": full_name,
+                    "person_id": person_id
+                })
+                await OrcidController.insert(orcid_item)
+
 
 async def get_orcid_list():
-    orcid_list_cleaned = []
-
-    file = open(ASSETS_JSON_TMP, "r")
-    persons_assets = json.loads(file.read())
-
-    # ldap_list = get_ldap_list_persons()[0:5]
-    # assets_list = persons_assets["hydra:member"][0:5]
-
-    ldap_list = get_ldap_list_persons()
-    assets_list = persons_assets["hydra:member"]
-
-    for item in assets_list:
-        name = item['nombre']
-        lastName = item['apellido1'] + ' ' + item['apellido2']
-        assets_full_name = name + ' ' + lastName
+    persons = await PersonsController.retrieve()
+    for person in persons:
 
         # wait a time before execute query for assets orcid
         sleep_time = randint(3, 9)
         print('sleep {0} seconds'.format(sleep_time))
         time.sleep(sleep_time)
-        orcid_assests_list = get_orcid_list_by_name(name, lastName)
 
-        if orcid_assests_list:
-            for orcid_item in orcid_assests_list:
-                # if orcid_item and 'given-names' in orcid_item.keys() and 'family-names' in orcid_item.keys():
-                #     given_names = orcid_item['given-names'] if orcid_item['given-names'] else ''
-                #     family_names = orcid_item['family-names'] if orcid_item['family-names'] else ''
-                #     orcid_full_name = given_names + ' ' + family_names
+        ldap_name = person['ldap_name']
+        ldap_lastName = person['ldap_lastName']
 
-                    # if orcid_item['orcid-id'] not in orcid_list_cleaned and orcid_full_name.lower() == assets_full_name.lower():
-                    if orcid_item['orcid-id'] not in orcid_list_cleaned:
-                        orcid_list_cleaned.append(orcid_item['orcid-id'])
-                        await OrcidController.insert(orcid_item)
+        await update_orcid_by_person(
+            person['_id'],
+            get_orcid_list_by_name_and_last_name(ldap_name, ldap_lastName)
+        )
 
-    for dn, entry in ldap_list:
-        if entry and 'sn' in entry.keys() and 'givenName' in entry.keys() and 'displayName' in entry.keys():
-            given_names = entry['givenName'][0].decode("utf-8")
-            family_name = entry['sn'][0].decode("utf-8")
+        for alias in person['aliases']:
+            orcid_list = get_orcid_list_by_full_name(alias)
+            await update_orcid_by_person(person['_id'], orcid_list)
 
-            # wait a time before execute query for ldap orcid
-            sleep_time = randint(3, 9)
-            print('sleep {0} seconds'.format(sleep_time))
-            time.sleep(sleep_time)
-            orcid_ldap_list = get_orcid_list_by_name(given_names, family_name)
+        orcid_list = await OrcidController.retrieve({"person_id": person['_id']})
 
-            if orcid_ldap_list:
-                for orcid_item in orcid_ldap_list:
-                    # if orcid_item and 'given-names' in orcid_item.keys() and 'family-names' in orcid_item.keys():
-                        # given_names = orcid_item['given-names'] if orcid_item['given-names'] else ''
-                        # family_names = orcid_item['family-names'] if orcid_item['family-names'] else ''
+        for orcid_item in orcid_list:
+            if person['email'] in get_email_by_orcid(orcid_item['orcid_id']):
+                print('UPDATE ORCID_ID BY PERSON EMAIL')
+                print("=========================")
+                await PersonsController.update_person(person['_id'], dict(orcid=orcid_item['orcid_id']))
+            # elif orcid_item['full_name'] in persons['aliases']:
 
-                        # orcid_full_name = given_names + ' ' + family_names
-                        # ldap_displayName = entry['displayName'][0].decode("utf-8")
 
-                        # if orcid_item['orcid-id'] not in orcid_list_cleaned and orcid_full_name == ldap_displayName:
-                        if orcid_item['orcid-id'] not in orcid_list_cleaned:
-                            orcid_list_cleaned.append(orcid_item['orcid-id'])
-                            await OrcidController.insert(orcid_item)
 
-    f_ad = open("demofile4.json", "a")
-    f_ad.write(str(orcid_list_cleaned))
-    f_ad.close()
+    # f_ad = open("demofile4.json", "a")
+    # f_ad.write(str(orcid_list_cleaned))
+    # f_ad.close()
 
 
 async def get_orcid():
     # persons = await PersonsController.retrieve()
-    persons = await PersonsController.retrieve_person("10642          ")
+    persons = await PersonsController.retrieve_person({"idvalue": "10642"})
     for person in [persons]:
         fullNameSPI = person['name'] + ' ' + person['lastName']
         orcid_list = get_orcid_list()
